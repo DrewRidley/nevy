626fb90b2443c5dda7694e3824480a49986a6729
Drew Ridley <drew.ridley03@gmail.com>
2024-01-23 15:33:05 -0500

    refactor of policies

--- src/policy.rs
+++ src/policy.rs
@@ -1,175 +1,175 @@
-use std::hash::Hash;
+//! Contains all types and systems associated with policies.
+//! Policies are used to control when entity updates should be dispatched, and who shall receive them.
+//! These are useful to dynamically hide game state to eliminate some forms of cheating,
+//! or to reduce bandwidth usage.
+//! 
+//! The two primary interfaces are a [ComponentPolicy] or an [EntityRingPolicy].
+//! 
+//! The former is used to declare which clients shall receive a particular component,
+//! while the latter is used to establish rings of influence around a particular entity,
+//! and the maximum update rate for each ring.
+use std::{marker::PhantomData, time::Duration};
+use bevy::ecs::component::Component;
+use num_traits::Num;
 use smallvec::SmallVec;
+use crate::ClientId;
 
 
-/// A marker trait indicating that a given type can be used as a client identifier.
-/// This will be auto-implemented on all types that implement [Hash], [Send] and [Sync].
-/// [Send] and [Sync] are required because the underlying network buffer is potentially shared between threads.
-pub trait ClientId: Hash + Eq + Send + Sync { }
 
-//Implement ClientId for all hashable types.
-impl<T: Hash + Eq + Send + Sync> ClientId for T { }
-
-
-/// A policy dictating which clients shall receive a particular piece of state.
+/// The clients selected by this particular policy.
 /// 
-/// Particularly useful in games where some information must be hidden from the players.
-/// For example, in a PvP game, you might want to hide the health of the enemy team to a player.
-/// It is possible to set a single policy for an entire entity with [EntityRef::set_net_policy].
-/// 
-/// # Type Parameter
-/// `I`: The type of the client identifier.
-#[derive(Hash)]
-pub enum ClientPolicy<I:  ClientId> {
-    /// Synchronizes this state with all clients.
+/// Use this to change over time which entities are included in the policy.
+#[derive(PartialEq, Eq)]
+pub enum PolicyType<I: ClientId> {
     All,
-    /// Synchronizes this state with all EXCEPT the given clients.
     Exclude(SmallVec<[I; 32]>),
-    /// Synchronizes this state with only the given clients.
-    // Here, we avoid inlining because in most cases the inclusionary set will be quite large.
     Include(Vec<I>),
-    /// Synchronizes this state with only the given client.
-    One(I),
-    /// Synchronize this unit of state exclusively with the owner.
-    /// This will not synchronize if the entity does not have a marked client as the owner.
     Owner,
-    /// Disable synchronization of this component (temporarily).
     None
 }
 
-
-macro_rules! consuming_method {
-    ($method_name:ident ($($arg:ident: $type:ty),*) -> $return_type:ty) => {
-        fn $method_name(self $(, $arg: $type)*) -> $return_type
-        where
-            Self: Sized,
-        {
-            match self {
-                Self::All(iter) => iter.$method_name($($arg),*),
-                Self::Exclude(iter) => iter.$method_name($($arg),*),
-                Self::Include(iter) => iter.$method_name($($arg),*),
-                Self::One(iter) => iter.$method_name($($arg),*),
-                Self::Owner(iter) => iter.$method_name($($arg),*),
-                Self::None => ::core::iter::empty::<<Self as ::core::iter::Iterator>::Item>().$method_name($($arg),*),
-            }
+impl<I: ClientId> PolicyType<I> {
+    pub fn is_included(&self, id: &I) -> bool {
+        match self {
+            PolicyType::All => true,
+            PolicyType::Exclude(exclude) => !exclude.contains(id),
+            PolicyType::Include(include) => include.contains(id),
+            PolicyType::Owner => false,
+            PolicyType::None => false
         }
-    };
-}
+    }
 
-macro_rules! consuming_ord_method {
-    ($method_name:ident ($($arg:ident: $type:ty),*) -> $return_type:ty) => {
-        fn $method_name(self $(, $arg: $type)*) -> $return_type
-        where
-            Self: Sized,
-            T: Ord,
-        {
-            match self {
-                Self::All(iter) => iter.$method_name($($arg),*),
-                Self::Exclude(iter) => iter.$method_name($($arg),*),
-                Self::Include(iter) => iter.$method_name($($arg),*),
-                Self::One(iter) => iter.$method_name($($arg),*),
-                Self::Owner(iter) => iter.$method_name($($arg),*),
-                Self::None => ::core::iter::empty::<<Self as ::core::iter::Iterator>::Item>().$method_name($($arg),*),
-            }
+    pub fn is_excluded(&self, id: &I) -> bool {
+        match self {
+            PolicyType::All => false,
+            PolicyType::Exclude(exclude) => exclude.contains(id),
+            PolicyType::Include(include) => !include.contains(id),
+            PolicyType::Owner => true,
+            PolicyType::None => true
         }
-    };
-}
+    }
 
-macro_rules! immutable_method {
-    ($method_name:ident ($($arg:ident: $type:ty),*) -> $return_type:ty) => {
-        fn $method_name(&self $(, $arg: $type)*) -> $return_type {
-            match self {
-                Self::All(iter) => iter.$method_name($($arg),*),
-                Self::Exclude(iter) => iter.$method_name($($arg),*),
-                Self::Include(iter) => iter.$method_name($($arg),*),
-                Self::One(iter) => iter.$method_name($($arg),*),
-                Self::Owner(iter) => iter.$method_name($($arg),*),
-                Self::None => ::core::iter::empty::<<Self as ::core::iter::Iterator>::Item>().$method_name($($arg),*),
-            }
+    pub fn include_all(&mut self) {
+        *self = PolicyType::All;
+    }
+
+    pub fn exclude_all(&mut self) {
+        *self = PolicyType::None;
+    }
+
+    pub fn include(&mut self, id: I) {
+        match self {
+            PolicyType::All => {},
+            PolicyType::Exclude(exclude) => {
+                if let Some(index) = exclude.iter().position(|x| *x == id) {
+                    exclude.remove(index);
+                }
+            },
+            PolicyType::Include(include) => {
+                if !include.contains(&id) {
+                    include.push(id);
+                }
+            },
+            PolicyType::Owner => {},
+            PolicyType::None => {}
         }
-    };
-}
+    }
 
-macro_rules! mutable_method {
-    ($method_name:ident ($($arg:ident: $type:ty),*) -> $return_type:ty) => {
-        fn $method_name(&mut self $(, $arg: $type)*) -> $return_type {
-            match self {
-                Self::All(iter) => iter.$method_name($($arg),*),
-                Self::Exclude(iter) => iter.$method_name($($arg),*),
-                Self::Include(iter) => iter.$method_name($($arg),*),
-                Self::One(iter) => iter.$method_name($($arg),*),
-                Self::Owner(iter) => iter.$method_name($($arg),*),
-                Self::None => ::core::iter::empty::<<Self as ::core::iter::Iterator>::Item>().$method_name($($arg),*),
-            }
+    pub fn exclude(&mut self, id: I) {
+        match self {
+            PolicyType::All => {},
+            PolicyType::Exclude(exclude) => {
+                if !exclude.contains(&id) {
+                    exclude.push(id);
+                }
+            },
+            PolicyType::Include(include) => {
+                if let Some(index) = include.iter().position(|x| *x == id) {
+                    include.remove(index);
+                }
+            },
+            PolicyType::Owner => {},
+            PolicyType::None => {}
         }
-    };
+    }
+
+    pub fn toggle(&mut self, id: I) {
+        match self {
+            PolicyType::All => {},
+            PolicyType::Exclude(exclude) => {
+                if let Some(index) = exclude.iter().position(|x| *x == id) {
+                    exclude.remove(index);
+                } else {
+                    exclude.push(id);
+                }
+            },
+            PolicyType::Include(include) => {
+                if let Some(index) = include.iter().position(|x| *x == id) {
+                    include.remove(index);
+                } else {
+                    include.push(id);
+                }
+            },
+            PolicyType::Owner => {},
+            PolicyType::None => {}
+        }
+    }
 }
 
-#[derive(Clone, Debug)]
-pub enum PolicyIter<T, I1, I2, I3, I4, I5>
-where
-    I1: Iterator<Item = T>,
-    I2: Iterator<Item = T>,
-    I3: Iterator<Item = T>,
-    I4: Iterator<Item = T>,
-    I5: Iterator<Item = T>,
-{
-    All(I1),
-    Exclude(I2),
-    Include(I3),
-    One(I4),
-    Owner(I5),
-    None,
+/// A overarching policy guiding how and when the component 'C' shall be streamed on a particular entity.
+/// 
+/// If all networked components contain an identical policy, the entire entity will be synchronized
+/// according to that policy.
+/// 
+/// This is the default stream policy. For limiting refresh rate or other customized stream behavior,
+/// Write your own policy and ensure that it impls [StreamDecision].
+#[derive(Component, PartialEq, Eq)]
+pub struct ComponentPolicy<I: ClientId, C: Component> {
+    pub component: PhantomData<C>,
+    pub policy: PolicyType<I>,
 }
 
-impl<T, I1, I2, I3, I4, I5> Iterator for PolicyIter<T, I1, I2, I3, I4, I5>
-where
-    I1: Iterator<Item = T>,
-    I2: Iterator<Item = T>,
-    I3: Iterator<Item = T>,
-    I4: Iterator<Item = T>,
-    I5: Iterator<Item = T>,
-{
-    type Item = T;
+impl<I: ClientId, C: Component> ComponentPolicy<I, C> {
+    pub fn new(pol: PolicyType<I>) -> Self {
+        Self {
+            component: PhantomData,
+            policy: pol
+        }
+    }
 
-    mutable_method!(next() -> Option<Self::Item>);
-    consuming_method!(count() -> usize);
-    consuming_method!(last() -> Option<Self::Item>);
-    consuming_ord_method!(max() -> Option<Self::Item>);
-    consuming_ord_method!(min() -> Option<Self::Item>);
-    mutable_method!(nth(n: usize) -> Option<Self::Item>);
-    immutable_method!(size_hint() -> (usize, Option<usize>));
+    /// Gets a mutable reference to the underlying policy.
+    pub fn policy_mut(&mut self) -> &mut PolicyType<I> {
+        &mut self.policy
+    }
+
+    /// Gets a reference to the underlying policy.
+    pub fn policy(&self) -> &PolicyType<I> {
+        &self.policy
+    }
 }
 
-impl<T, I1, I2, I3, I4, I5> core::iter::FusedIterator for PolicyIter<T, I1, I2, I3, I4, I5>
-where
-    I1: core::iter::FusedIterator + Iterator<Item = T>,
-    I2: core::iter::FusedIterator + Iterator<Item = T>,
-    I3: core::iter::FusedIterator + Iterator<Item = T>,
-    I4: core::iter::FusedIterator + Iterator<Item = T>,
-    I5: core::iter::FusedIterator + Iterator<Item = T>,
-{
-}
 
-impl<T, I1, I2, I3, I4, I5> DoubleEndedIterator for PolicyIter<T, I1, I2, I3, I4, I5>
-where
-    I1: DoubleEndedIterator + Iterator<Item = T>,
-    I2: DoubleEndedIterator + Iterator<Item = T>,
-    I3: DoubleEndedIterator + Iterator<Item = T>,
-    I4: DoubleEndedIterator + Iterator<Item = T>,
-    I5: DoubleEndedIterator + Iterator<Item = T>,
-{
-    mutable_method!(next_back() -> Option<Self::Item>);
-    mutable_method!(nth_back(n: usize) -> Option<Self::Item>);
-}
+/// A policy that can optionally be associated with a client and a particular 'ring'.
+/// Any other entities that lie within 'N' distance to this one will receive
+/// the state of this entity, but will be limited by refresh rate.
+/// 
+/// If at least one ring is specified on an entity, all component policies will be adjusted to exclude
+/// clients once they fall off from the final ring.
+/// 
+/// It is logically invalid to have multiple rings with the same 'N' value. 
+/// 
+/// It is strongly advised to avoid having multiple rings with an identical 'max_refresh_rate' value,
+/// as this negatively affects performance.
+/// 
+/// /// # Type Parameters
+/// `N`: The numeric type used to represent distance.
+#[derive(Component)]
+pub struct EntityRingPolicy<N: Num, P> {
+    pub ring: N,
+    pub max_refresh_rate: Duration,
 
-impl<T, I1, I2, I3, I4, I5> ExactSizeIterator for PolicyIter<T, I1, I2, I3, I4, I5>
-where
-    I1: ExactSizeIterator + Iterator<Item = T>,
-    I2: ExactSizeIterator + Iterator<Item = T>,
-    I3: ExactSizeIterator + Iterator<Item = T>,
-    I4: ExactSizeIterator + Iterator<Item = T>,
-    I5: ExactSizeIterator + Iterator<Item = T>,
-{
-    immutable_method!(len() -> usize);
+    /// The associated marker for this policy.
+    /// For each (ring, Duration) pair, there should exist a unique marker type.
+    /// This allows you to have multiple rings on a single entity.
+    pol: PhantomData<P>
 }
