a348366a68af71d680db5dc438d1ca8ec70ed3a9
Drew Ridley <drew.ridley03@gmail.com>
2024-01-23 16:15:00 -0500

    Added new trait bounds for ClientId.

--- src/lib.rs
+++ src/lib.rs
@@ -43,45 +43,13 @@ use std::hash::Hash;
 
 pub mod policy;
 pub mod stream;
-
-
-/// A marker trait to indicate which client 'owns' this entity.
-/// Does not have to be exclusive. A single client can own multiple entities.
-/// In this context, 'owns' does not refer to authority, but rather, 
-/// which client should receive a particular piece of state.
-/// 
-/// # Type Parameter
-/// `I`: The type of the client identifier.
-#[derive(Component)]
-#[component(storage = "SparseSet")]
-pub struct Owner<I: ClientId>(pub I);
-
-/// A marker trait to indicate that a particular client is Authoritative over the state of this entity.
-///
-/// A client will dispatch state updates to other clients if it is 'self-authoritative'.
-/// Clients will reject state updates from non-authoritative clients (if a p2p transport layer is used).
-/// This marker is not used if there is a single authoritative server.
-/// A custom consensus mechanism may be required, depending on the game requirements.
-/// 
-/// # Type Parameter
-/// `I`: The type of the client identifier.
-#[derive(Component)]
-#[component(storage = "SparseSet")]
-pub struct Authoritative<I: ClientId>(pub I);
-
-/// A marker to indicate that the entity is networked.
-/// This will likely rarely be used but exists if you need your logic to be aware of the networked status of an entity.
-/// This is shorthand for checking each of the many components for an adjacent [NetComp]/[DynNetComp].
-/// Due to its sparse nature, its only recommended to use if you require absolute proof of networked state.
-#[derive(Component)]
-#[component(storage = "SparseSet")]
-pub struct NetEntity;
+pub mod messages;
 
 
 /// A marker trait indicating that a given type can be used as a client identifier.
 /// This will be auto-implemented on all types that implement [Hash], [Send] and [Sync].
 /// [Send] and [Sync] are required because the underlying network buffer is potentially shared between threads.
-pub trait ClientId: Hash + Eq + Send + Sync { }
+pub trait ClientId: Hash + Eq + Send + Sync + Clone { }
 
 //Implement ClientId for all hashable types.
-impl<T: Hash + Eq + Send + Sync> ClientId for T { }
+impl<T: Hash + Eq + Send + Sync + Clone> ClientId for T { }
