f8d21f5dd33c3c1b6d78da835da4bc0acc55e57b
Drew Ridley <drew.ridley03@gmail.com>
2024-06-16 15:12:06 -0400

    Initial wasm

--- crates/nevy_wasm/src/connection.rs
+++ crates/nevy_wasm/src/connection.rs
@@ -5,11 +5,19 @@ use std::{
     pin::Pin,
 };
 
+use futures_lite::stream::race;
 use slotmap::new_key_type;
 use transport_interface::{ConnectionMut, ConnectionRef, StreamEvent};
-use web_transport_wasm::{Session, WebError};
+use web_sys::{
+    wasm_bindgen::{closure::Closure, JsCast, JsValue},
+    WebTransportBidirectionalStream, WebTransportReceiveStream, WebTransportSendStream,
+};
+use web_transport_wasm::Session;
 
-use crate::stream::WasmStreamId;
+use crate::{
+    reader::{Reader, WebError},
+    stream::WasmStreamId,
+};
 
 new_key_type! {
     pub struct WasmConnectionId;
@@ -17,12 +25,12 @@ new_key_type! {
 
 struct ConnectedWasmSession {
     /// The underlying wasm session used to establish new streams, read, or write data.
-    session: Session,
+    session: web_sys::WebTransport,
     /// A future that accepts either unidirectional or bidirectional streams from the peer.
     /// This future will always exist in the [WasmSession::Connected] state and must be polled accordingly.
     accept_future: Box<dyn Future<Output = ()>>,
 
-    /// A future used to receive the next chunk of data from the peer.
+    /// A future used to populate the internal recv buffers from the async methods.
     recv_future: Box<dyn Future<Output = ()>>,
     /// A future used to progress all outstanding writes.
     send_future: Box<dyn Future<Output = ()>>,
@@ -42,6 +50,8 @@ pub struct WasmConnection {
     /// The session. May or may not contain a valid session depending on the state.
     session: WasmSession,
     /// A collection of events associated with this connection that can be read by the manager process.
+    /// This is disconnected from the session state because stream events (such as disconnected) can still be read
+    /// even if the connection is no longer valid.
     stream_events: VecDeque<StreamEvent<WasmConnectionId>>,
 }
 
@@ -50,6 +60,20 @@ impl WasmConnection {
         WasmConnection {
             session: WasmSession::Disconnected,
             stream_events: VecDeque::new(),
+        }
+    }
+
+    async fn accept_uni(&mut self) {}
+
+    async fn accept_bi(&mut self) -> Result<WebTransportBidirectionalStream, WebError> {
+        if let WasmSession::Connected(session) = self.session {
+            let transport = session.session;
+
+            let mut reader = Reader::new(&transport.incoming_bidirectional_streams())?;
+            let stream: WebTransportBidirectionalStream =
+                reader.read().await?.expect("Closed without error");
+
+            Ok(stream)
         }
     }
 }
