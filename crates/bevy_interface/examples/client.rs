c6d69f722b285fdb0295fe326a5d3734777614fd
Jacob-JB <jacobburakowski@gmail.com>
2024-06-18 18:37:22 +1000

    removed unnecessary server config from bevy interface example client

--- crates/bevy_interface/examples/client.rs
+++ crates/bevy_interface/examples/client.rs
@@ -32,53 +32,8 @@ struct ExampleStream {
     buffer: Vec<u8>,
 }
 
-fn load_certs() -> rustls::ServerConfig {
-    let chain = std::fs::File::open("fullchain.pem").expect("failed to open cert file");
-    let mut chain: std::io::BufReader<std::fs::File> = std::io::BufReader::new(chain);
-
-    let chain: Vec<rustls::pki_types::CertificateDer> = rustls_pemfile::certs(&mut chain)
-        .collect::<Result<_, _>>()
-        .expect("failed to load certs");
-    let mut keys = std::fs::File::open("privkey.pem").expect("failed to open key file");
-
-    let mut buf = Vec::new();
-    keys.read_to_end(&mut buf).unwrap();
-
-    let key = rustls_pemfile::private_key(&mut std::io::Cursor::new(&buf))
-        .expect("failed to load private key")
-        .expect("missing private key");
-
-    let config = rustls::ServerConfig::builder_with_provider(Arc::new(
-        rustls::crypto::ring::default_provider(),
-    ))
-    .with_protocol_versions(&[&rustls::version::TLS13])
-    .unwrap()
-    .with_no_client_auth()
-    .with_single_cert(chain, key)
-    .unwrap();
-
-    config
-}
-
 fn spawn_endpoint(mut commands: Commands) {
-    let mut config = load_certs();
-
-    config.max_early_data_size = u32::MAX;
-    config.alpn_protocols = vec![b"h3".to_vec()]; // this one is important
-
-    let config: nevy_quic::quinn_proto::crypto::rustls::QuicServerConfig =
-        config.try_into().unwrap();
-
-    let mut server_config = nevy_quic::quinn_proto::ServerConfig::with_crypto(Arc::new(config));
-
-    let mut transport_config = nevy_quic::quinn_proto::TransportConfig::default();
-    transport_config.enable_segmentation_offload(false);
-    transport_config.max_idle_timeout(Some(Duration::from_secs(600).try_into().unwrap()));
-
-    server_config.transport = Arc::new(transport_config);
-
-    let endpoint =
-        QuinnEndpoint::new("0.0.0.0:0".parse().unwrap(), None, Some(server_config)).unwrap();
+    let endpoint = QuinnEndpoint::new("0.0.0.0:0".parse().unwrap(), None, None).unwrap();
 
     commands.spawn((ExampleEndpoint, BevyEndpoint::new(endpoint)));
 }
